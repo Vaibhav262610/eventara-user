7b1c831e36e347564ffca0af42795b793e66602a
mukulrajpoot262610 <branda56235@gmail.com>
2025-02-16 17:50:26 +0530

    H

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <h1>Eventara</h1>
+                <div>
+                    <h2>Events</h2>
+                    <h2>About</h2>
+                    <h2>Community</h2>
+                </div>
+                <div>
+                    <button>Organize a Event</button>
+                    <button>Sign In</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
