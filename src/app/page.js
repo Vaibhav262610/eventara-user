6e51897dad8e4cf978c5788101523456bf7dca9b
mukulrajpoot262610 <branda56235@gmail.com>
2025-02-18 20:30:17 +0530

    H

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import About from '@/components/About'
 import Header from '@/components/Header'
-import Marquee from '@/components/marquee'
+import Marquee from '@/components/Marquee'
 import React from 'react'
 
 const page = () => {
